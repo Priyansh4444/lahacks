d5becd25b6b57f0c409ed687f7a94d8f7f7a6bad
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-04-20 09:24:09 -0700

    much better background right?

--- src/components/wavy-background.tsx
+++ src/components/wavy-background.tsx
@@ -7,10 +7,10 @@ function Background({ children }: { children: React.ReactNode }){
       <video autoPlay loop muted style={{
         position: 'absolute',
         width: '100%',
+        objectFit: 'cover',
         left: '50%',
         top: '50%',
         height: '100%',
-        objectFit: 'cover',
         transform: 'translate(-50%, -50%)',
         zIndex: '-1', // Ensure video is in the background
       }}>
